40b28a51c9ac325ba793c00ff8184a2119b25ffb
mjgreene2010@gmail.com <mjgreene2010@gmail.com>
2018-09-06 15:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function dec(n){
 }
 
 function makeInt(n){
-  return parseInt('n', 10)
+  return parseInt(n, 10)
 }
 
-//function preserveDecimal(n){
- // return parseInt(hard, 10)
-//}
+function preserveDecimal(n){
+ return parseInt(hard, 10)
+}
